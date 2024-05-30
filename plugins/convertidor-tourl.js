367e15061337f2d35bd8fabc5bc0f99dfad4d467
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-05-30 18:14:42 -0300

    「⸙🥃⦑ノ🎉𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥ノ🎉⦒🪐⸙」

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,55 +1,25 @@
-import uploadFile from '../lib/uploadFile.js';
-import uploadImage from '../lib/uploadImage.js';
-const handler = async (m) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*⚠️ Responda A Una Imagen.*';
-  const media = await q.download();
-  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
-  const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`*📖 Enlace:* ${link}`);
-};
-handler.help = ['tourl <reply image>'];
-handler.tags = ['sticker'];
-handler.command = /^(upload|tourl)$/i;
-export default handler;
-
-
-/*Créditos a https://github.com/AzamiJs
-
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
-
-var handler = async (m) => {
-
+let handler = async (m) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
 let name = await conn.getName(who)
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
-m.react(done)
+if (!mime) throw `『🎉』𝙍𝙚𝙨𝙥𝙤𝙣𝙙𝙖 𝙖 𝙪𝙣𝙖 𝙞𝙢𝙖𝙜𝙚𝙣 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.`
 let media = await q.download()
 let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 let link = await (isTele ? uploadImage : uploadFile)(media)
-let info = ` *🗂️ ENLACE:*\n${link}\n
-*⚖️ TAMAÑO:*\n${media.length} bytes\n
-*🚀 EXPIRACION:*\n ${isTele ? '✅ NO EXPIRA' : '⚠️ DESCONOCIDO'}\n
-*🔰 ACORTADO:*\n${await shortUrl(link)}`
-
-conn.reply(m.chat, info, m, { contextInfo: { externalAdReply :{ mediaUrl: ig, mediaType: 2, title: wm, body: azami, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link}}})
-
-}
+let caption = `『🏙️』𝗘𝗡𝗟𝗔𝗖𝗘: ${link}\n『🌆』𝗣𝗘𝗦𝗢: ${media.length}\n『🌌』𝗘𝗫𝗣𝗜𝗥𝗔𝗖𝗜𝗢𝗡: ${isTele ? 'Sin expiración.' : 'Desconocido.'}`
+conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: channel, mediaType: 2, title: '꧁𝆭🎉𝆹𝅥ꥈ「𝙉ꥈ𝙖𝆭𝙪𝙛𝙧𝆭ꥈ𝙖𝙕𝙖𝆭𝙥ꥈ𝙥」ꥈ𝆹𝅥🎉𝆭꧂', body: '• Convertidor de enlaces.', thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
 handler.help = ['tourl']
-handler.tags = ['transformador']
+handler.tags = ['herramientas']
 handler.command = /^(tourl|upload)$/i
-
-handler.limit = true
-
 export default handler
 
 async function shortUrl(url) {
 let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
 return await res.text()
-}*/
+  }
+                                                                              
