4d42295808440e676189e98b44fe075f739ee4c1
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-05-30 16:09:27 -0300

    「⸙🥃⦑ノ🎉𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥ノ🎉⦒🪐⸙」

--- plugins/descargar-vd.js
+++ plugins/descargar-vd.js
@@ -1,24 +1,24 @@
-
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 	
-  if (!text) throw `*${usedPrefix + command}* Erika Lundmeon - Yad`
+  if (!text) throw `『🪐』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙩𝙚𝙭𝙩𝙤 𝙤 𝙚𝙣𝙡𝙖𝙘𝙚 𝙙𝙚 𝙔𝙤𝙪𝙏𝙪𝙗𝙚 𝙫𝙖𝙡𝙞𝙙𝙤.\n\n𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n• *${usedPrefix + command} Erika Lundmeon - Yad*\n• *${usedPrefix + command} https://youtube.com/watch?v=NMxZcxbQ-Zg*`
 	let res = await yts(text)
 	let vid = res.videos[0]
 	if (!vid) throw `No encontrado`
 	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
-	m.react('🎬') 
+	m.react('🎉') 
   let play = `
-Titulo: ${vid.title}
-Publicado: ${vid.ago}
-Duración: ${vid.timestamp}
-Vistas: ${vid.views.toLocaleString()}`
+『👀』𝙀𝙣𝙘𝙤𝙣𝙩𝙧𝙖𝙙𝙤!!! 🎉
+▫ •─────────────• ▫ 
+𝙋𝙖𝙧𝙖 𝙫𝙚𝙧 𝙢𝙖𝙨 𝙫𝙞𝙙𝙚𝙤𝙨 𝙘𝙤𝙢𝙤
+𝙚𝙨𝙩𝙚, 𝙪𝙨𝙚 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤:
+${usedPrefix}playlist ${vid.title}`
  await conn.sendButton(m.chat, play, wm, thumbnail, [
-    ['🔊 AUDIO 🔊', `${usedPrefix}playdoc ${url}`],
-    ['🎬 VIDEO 🎬', `${usedPrefix}playdoc2 ${url}`],
-    ['✅ Auto Verify ✅', `${usedPrefix}reg subNZ.18`],
-    ['🎒 Menu Completo 🎒', `${usedPrefix}menucompleto`],
+    ['𝐀𝐔𝐃𝐈𝐎 🪐', `${usedPrefix}playaudio ${url}`],
+    ['𝐕𝐈𝐃𝐄𝐎 🪐', `${usedPrefix}playvideo ${url}`],
+    ['𝐀𝐔𝐃𝐈𝐎 𝐃𝐎𝐂 📦', `${usedPrefix}playdoc ${url}`],
+    ['𝐕𝐈𝐃𝐄𝐎 𝐃𝐎𝐂 📦', `${usedPrefix}playdoc2 ${url}`],
  ], null, [['Canal', `${channel}`]], m)
 }
 handler.help = ['play']
@@ -27,4 +27,5 @@ handler.command = ['dlav', 'downav']
 handler.disabled = false
 
 export default handler
-   
+
+	 
