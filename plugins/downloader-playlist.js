d890680208c14c52140dfca8c915290297303e57
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-05-30 21:40:44 -0300

    「⸙🥃⦑ノ🎉𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥ノ🎉⦒🪐⸙」

--- plugins/downloader-playlist.js
+++ plugins/downloader-playlist.js
@@ -1,51 +1,43 @@
 import yts from 'yt-search';
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-if (!text) conn.reply(m.chat, `『🪐』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙡𝙖 𝙘𝙖𝙣𝙘𝙞𝙤𝙣 𝙦𝙪𝙚 𝙗𝙪𝙨𝙘𝙖.\n\n𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n• *${usedPrefix + command} Erika lundmoen - yad*`, m)
-try {
-    let result = await yts(text);
-    let ytres = result.videos;
-  let teskd = `LISTA DE: *${text}*`
-    
-let listSections = [];
-for (let index in ytres) {
-        let v = ytres[index];
-        listSections.push({
-         title: `${wm}`,
-            rows: [
-                {
-                    header: '🎬 𝐕𝐈𝐃𝐄𝐎 🎬',
-                    title: "",
-                    description: `${v.title} • ${v.timestamp}\n`, 
-                    id: `${usedPrefix}playvideo ${v.url}`
-                },
-                {
-                    header: "🎶 𝐀𝐔𝐃𝐈𝐎 🎶",
-                    title: "" ,
-                    description: `${v.title} • ${v.timestamp}\n`, 
-                    id: `${usedPrefix}playaudio ${v.url}`
-                }, 
-              {
-                    header: "📦 𝐕𝐈𝐃𝐄𝐎 𝐃𝐎𝐂 📦",
-                    title: "" ,
-                    description: `${v.title} • ${v.timestamp}\n`, 
-                    id: `${usedPrefix}playdoc2 ${v.url}`
-                }, 
-                {
-                    header: "📦 𝐀𝐔𝐃𝐈𝐎 𝐃𝐎𝐂 📦",
-                    title: "" ,
-                    description: `${v.title} • ${v.timestamp}\n`, 
-                    id: `${usedPrefix}playdoc ${v.url}`
-                }
-            ]
-        });
+import fs from 'fs';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  if (!text) throw `*[❗] Nombre de la canción faltante, por favor ingrese el comando más el nombre/titulo de una canción*\n\n*—◉ Ejemplo:*\n*${usedPrefix + command} Begin you*`;
+  try {
+    const vids_ = {
+      from: m.sender,
+      urls: [],
+    };
+    if (!global.videoList) {
+      global.videoList = [];
     }
-await conn.sendList(m.chat, `DESCARGADOR\n`, `\nLISTA DE: *${text}*`, `Ver Lista`, listSections, m);
-} catch (e) {
-await conn.sendButton(m.chat, `Ocurrio un error inesperado.`, null, [['❌ REPORTAR ❌', `#reporte El comando *${usedPrefix + command}* tiene un error`]], null, null, m)
-console.log(e) 
-}}
-handler.help = ['playlist']
-handler.tags = ['dl']
-handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+    if (global.videoList[0]?.from == m.sender) {
+      global.videoList.splice(0, global.videoList.length);
+    }
+    const results = await yts(text);
+    const textoInfo = `*[❗] Puedes descargar el video que quieras de la siguiente forma:*
+◉ ${usedPrefix}audio <numero>
+◉ ${usedPrefix}video <numero> 
 
-export default handler
+*—◉ Ejemplos:*
+*◉ ${usedPrefix}audio 5*
+*◉ ${usedPrefix}video 8*`.trim();
+    const teks = results.all.map((v, i) => {
+      const link = v.url;
+      vids_.urls.push(link);
+      return `[${i + 1}] ${v.title}
+↳ 🫐 *_Link :_* ${v.url}
+↳ 🕒 *_Duración :_* ${v.timestamp}
+↳ 📥 *_Subido :_* ${v.ago}
+↳ 👁 *_Vistas :_* ${v.views}`;
+    }).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+    conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, m);
+    global.videoList.push(vids_);
+  } catch {
+    await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*');
+  }
+};
+handler.help = ['playlist *<texto>*'];
+handler.tags = ['search'];
+handler.command = /^playlist|playlist2$/i;
+export default handler;
+                 
