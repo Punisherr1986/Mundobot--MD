312c817d4c9291d68e576ae419116e4be0fdbc50
NaufraZapp <161604719+NaufraZapp@users.noreply.github.com>
2024-06-07 09:37:25 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,11 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`『✦』Hola: @${m.sender.split`@`[0]}, lamento esto, pero no esta admitido hablar en el privado.`, false, {mentions: [m.sender]});
+    await m.reply(`『✦』Hola: @${m.sender.split`@`[0]}, *Le saluda NaufraZapp*
+> Disculpe pero no está permitido hablar en privado del bot, así que tengo el deber de bloquearlo
+
+*Si quieres saber como instalarme gratuitamente para tenerme en su grupo, por favor siga el siguiente canal de WhatsApp*
+https://whatsapp.com/channel/0029VaRibRvDuMRj6ozMSN1l`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
