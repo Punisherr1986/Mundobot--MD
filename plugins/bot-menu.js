a8be5074b8ed1b74d97fcc7be81bb94254647faf
Punisherr1986 <gallardocortesjuan1986@gmail.com>
2024-06-26 22:08:03 +0200

    Update bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -7,9 +7,10 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
-let estado = `Bienvenido a 𝙉𝙖𝙪𝙛𝙧𝙖𝙕𝙖𝙥𝙥-𝙈𝘿:`
+let estado = `Bienvenido a MundoBot-MB:`
 await conn.sendButton(m.chat, estado, `Selecciona los siguientes botones 🛎️ y no te olvides de verificarte.`, pp, [
 ['𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 💥', '.menucompleto'],
+['GRUPOS DE BOTS', '.quierounbot'],
 ['𝘼𝙐𝙏𝙊 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 ✅', '#reg nauSub.20'],
 ['🛒 𝗖𝗢𝗠𝗣𝗥𝗔𝗥 𝗕𝗢𝗧 🛒', '#installbot']], null, [
 ['𝘾𝘼𝙉𝘼𝙇 𝙊𝙁𝙄𝘾𝙄𝘼𝙇🌎', `${channel}`]], m)
