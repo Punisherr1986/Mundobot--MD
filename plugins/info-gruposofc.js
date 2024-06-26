475b7bd8209f52efe3e2b88a4029651d7ebecd4c
Punisherr1986 <gallardocortesjuan1986@gmail.com>
2024-06-26 09:02:06 +0200

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,16 +1,18 @@
-let media = './Menu7.png'
+hlet media = './Menu7.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '👀', key: m.key } })
 let str = `*💥 GRUPOS OFICIALES🐦‍🔥*
 
-    *Naufrazapp oficial_*
-  *❏ https://chat.whatsapp.com/IxdHYBEnUJ5Eiu3qiDjCTal*
+    *_MundoBot oficial_*
+  *❏ https://chat.whatsapp.com/FkjjdPygRntIhOtbJ7MwkG*
 *_Unete_*
 
-    *_grupo bot Naufrazapp-MD_*
-  *_❏ https://chat.whatsapp.com/Elot5C62z0AGvxwZjXsW4j*
-*_Unete_*
+    *_grupo bot 1 MundoBot-MD_*
+  *_❏ https://chat.whatsapp.com/GLmQrA3ehcQD35Yh77xp8x*
+
+*_grupo bot 2 MundoBot-MD_*
+https://chat.whatsapp.com/J2HUC0jWtec5wtY3OWyzVb
 
   *💥 CANAL OFICIAL🐦‍🔥*
 
