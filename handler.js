ce8115c1c3daa4d8344af0caecb18d826b305859
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2024-06-27 18:33:35 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -989,6 +989,7 @@ export async function handler(chatUpdate) {
           simi: false,
           game: false,
           expired: 0,
+          stickers: true,
         };
       }
       const settings = global.db.data.settings[this.user.jid];
